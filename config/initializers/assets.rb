0dd81b93ceb44ac80c343eb93a6d29533453c286
Mike <mclancy96@gmail.com>
2025-09-08 11:06:42 -0400

    Setup fun font

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,3 +7,9 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+# Add fonts directory to asset paths
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+# Precompile font files
+Rails.application.config.assets.precompile += %w[*.ttf *.woff *.woff2 *.eot]
