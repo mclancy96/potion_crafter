d105aa931f67b9dcbcd5c5d99a506753ac422181
Mike <mclancy96@gmail.com>
2025-09-08 12:10:54 -0400

    Remove email column

---
+++ db/migrate/20250908160942_drop_email_column_on_user_table.rb
@@ -0,0 +1,5 @@
+class DropEmailColumnOnUserTable < ActiveRecord::Migration[8.0]
+  def change
+    remove_column :users, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_08_134410) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_08_160942) do
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -53,7 +53,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_08_134410) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
