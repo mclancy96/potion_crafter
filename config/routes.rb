91ffc549ec3bd91ef2f0f7ea69264f855cd14022
Mike <mclancy96@gmail.com>
2025-09-08 10:44:26 -0400

    Appease rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :potions do
     resources :ingredients
--- db/migrate/20250908134326_create_potion_ingredients.rb
+++ db/migrate/20250908134326_create_potion_ingredients.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreatePotionIngredients < ActiveRecord::Migration[8.0]
   def change
     create_table :potion_ingredients do |t|
