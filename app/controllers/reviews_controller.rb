9766d9d7fbc4669e6e5d8b8f9af0f3a376d9b06d
Mike <mclancy96@gmail.com>
2025-09-10 08:47:21 -0400

    Display working review form on potion reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,37 @@
 
 class ReviewsController < ApplicationController
   before_action :require_logged_in
+  before_action :set_potion
+
+  def index
+    @reviews = @potion.reviews
+    @review = Review.new
+    redirect_to potion_path(@potion) if @reviews.empty?
+  end
 
   def edit; end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.potion_id = @potion.id
+    if @review.save
+      flash[:notice] = "Review created successfully"
+      redirect_to potion_reviews_path(@potion)
+    else
+      @reviews = @potion.reviews
+      flash.now[:alert] = "Unable to Create Review"
+      render :index
+    end
+  end
+
+private
+
+  def set_potion
+    @potion = Potion.find(params[:potion_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:potion_id, :comment, :rating)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -9,7 +9,21 @@ class Review < ApplicationRecord
                             greater_than_or_equal_to: 1,
                             less_than_or_equal_to: 5 }
 
+  scope :most_recent, ->(limit = 3) { order(created_at: :desc).limit(limit) }
+
   def rating_stars
     "⭐️" * rating
+  end
+
+  def formatted_created_at
+    created_at.localtime.strftime("%b %d, %Y %I:%M %p")
+  end
+
+  def formatted_updated_at
+    updated_at.localtime.strftime("%b %d, %Y %I:%M %p")
+  end
+
+  def edited?
+    created_at != updated_at
   end
 end
