1e30cc4c55679bfacecbc121af23904fa1c4a6d7
Mike <mclancy96@gmail.com>
2025-09-10 12:58:58 -0400

    Add specs for potion_ingredient

--- spec/models/potion_ingredient_spec.rb
+++ spec/models/potion_ingredient_spec.rb
@@ -3,5 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe PotionIngredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+   it { should belong_to(:potion) }
+   it { should belong_to(:ingredient) }
+  end
+
+  describe "attribute validations" do
+    it 'is valid with a potion and ingredient' do
+      potion_ingredient = build(:potion_ingredient)
+      expect(potion_ingredient).to be_valid
+    end
+
+    it 'is invalid without a potion' do
+      potion_ingredient = build(:potion_ingredient, potion_id: nil)
+      expect(potion_ingredient).not_to be_valid
+    end
+
+    it 'is invalid without an ingredient' do
+      potion_ingredient = build(:potion_ingredient, ingredient_id: nil)
+      expect(potion_ingredient).not_to be_valid
+    end
+
+    it 'is invalid without an quantity' do
+      potion_ingredient = build(:potion_ingredient, quantity: nil)
+      expect(potion_ingredient).not_to be_valid
+    end
+
+  end
+
 end
