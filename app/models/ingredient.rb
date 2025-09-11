abc30961e5f9aa6793be383216062fe8d356e277
Mike <mclancy96@gmail.com>
2025-09-11 16:25:23 -0400

    Finish out review, potion, and ingredient styling

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -7,4 +7,19 @@ class Ingredient < ApplicationRecord
   validates :name, :description, :rarity, presence: true
   validates :name, uniqueness: { case_sensitive: false }
   validates :rarity, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+
+  def rarity_badge_color
+    case rarity
+    when 5
+      "danger"
+    when 4
+      "warning"
+    when 3
+      "info"
+    when 2
+      "primary"
+    else
+      "secondary"
+    end
+  end
 end
