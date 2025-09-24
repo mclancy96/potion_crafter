dded47044978ad436e1e2a51eccf7d1d7b95d03f
Mike <mclancy96@gmail.com>
2025-09-24 13:10:04 -0400

    Create spec for table component

--- app/components/potions/table_component.rb
+++ app/components/potions/table_component.rb
@@ -1,8 +1,9 @@
 module Potions
   class TableComponent < ViewComponent::Base
-    def initialize(potions:)
+    def initialize(potions:, current_user: nil)
       super()
       @potions = potions
+      @current_user = current_user
     end
   end
 end
